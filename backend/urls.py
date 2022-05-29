5cd8574c9c09f9da12dbbb95b3208d91e7890e40
simonkorl <mc21@mails.tsinghua.edu.cn>
2022-05-29 16:29:26 +0800

    feat: adjust to the latest django

--- backend/urls.py
+++ backend/urls.py
@@ -6,23 +6,9 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-
-from .api.views import index_view, MessageViewSet
-
-router = routers.DefaultRouter()
-router.register('messages', MessageViewSet)
 
 urlpatterns = [
-
-    # http://localhost:8000/
-    path('', index_view, name='index'),
-
-    # http://localhost:8000/api/<router-viewsets>
-    path('api/', include(router.urls)),
-
-    # http://localhost:8000/api/admin/
+    path('', include('mapi.urls')),
     path('api/admin/', admin.site.urls),
 ]
-
 
---
+++ mapi/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import index_view, MessageViewSet
+
+router = routers.DefaultRouter()
+router.register('messages', MessageViewSet)
+
+urlpatterns = [
+    # http://localhost:8000/
+    path('', index_view, name='index'),
+
+    # http://localhost:8000/api/<router-viewsets>
+    path('api/', include(router.urls)),
+]
